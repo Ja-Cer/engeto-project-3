dd40346a7f21ace9f3c8300ca0acf4420df13d0a
Ja-Cer <135021653+Ja-Cer@users.noreply.github.com>
2024-06-09 20:03:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+//Načtení prvků pro light mode swap
+const header = document.querySelector("header");
+const logo = document.querySelector(".logo");
+const aboutUs = document.querySelector(".about-us");
+const aboutUsText = document.querySelector(".about-us-text");
+const formApplication = document.querySelector(".formApplication");
+const form = document.querySelector("form");
+const footer = document.querySelector("footer");
+
+const rootStyles = getComputedStyle(document.documentElement);
+
+const headVarLight = rootStyles.getPropertyValue("--header-color-light");
+const headVarDark = rootStyles.getPropertyValue("--header-color-dark");
+
+const bodyVarLight = rootStyles.getPropertyValue("--body-color-light");
+const bodyVarDark = rootStyles.getPropertyValue("--body-color-dark");
+
+const formVarLight = rootStyles.getPropertyValue("--form-color-light");
+const formVarDark = rootStyles.getPropertyValue("--form-color-dark");
+
+//Načtení prvků pro Icon Swap
 const menuIcon = document.querySelector(".menu-icon");
 const menuList = document.querySelector("nav");
 const hamburgerIcon = document.querySelector(".fa-solid");
 
 const switchIcon = document.querySelector(".switch-icon");
 const lightIcon = document.querySelector(".fa-solid.fa-sun");
+
+//Načtení prvnků pro form password check display
+const formPassword = document.querySelector(".form-input-password");
+const formPasswordCheck = document.getElementById("formPasswordCheck");
+
+//Načtení prvků pro form alerts
+const formAlert = document.querySelector(".form-alert");
+
+//Načtení prvků pro form checks před odesláním
 
 // Funkce pro výměnu Icon
 const iconSwap = (iconAdd, iconRemove, location) => {
@@ -27,9 +57,66 @@ menuIcon.addEventListener("click", () => {
 switchIcon.addEventListener("click", () => {
   if (lightIcon.classList[1] === "fa-sun") {
     iconSwap("fa-cloud-moon", "fa-sun", lightIcon);
-
-    // //////// Doplnit přepínání stylů na "night mode"
+    logo.style.color = "white";
+    header.style.backgroundColor = headVarDark;
+    aboutUs.style.backgroundColor = bodyVarDark;
+    formApplication.style.backgroundColor = bodyVarDark;
+    form.style.backgroundColor = formVarDark;
+    aboutUsText.style.color = "white";
+    footer.style.backgroundColor = bodyVarDark;
   } else {
     iconSwap("fa-sun", "fa-cloud-moon", lightIcon);
+    logo.style.color = "black";
+    header.style.backgroundColor = headVarLight;
+    aboutUs.style.backgroundColor = bodyVarLight;
+    formApplication.style.backgroundColor = bodyVarLight;
+    form.style.backgroundColor = formVarLight;
+    aboutUsText.style.color = "black";
+    footer.style.backgroundColor = bodyVarLight;
+  }
+});
+
+//Password 2nd verification display
+formPassword.addEventListener("input", () => {
+  let lettersCount = formPassword.value.length;
+
+  console.log(lettersCount);
+
+  if (lettersCount > 0) {
+    formPasswordCheck.classList.remove("form-password-hidden");
+  } else {
+    formPasswordCheck.classList.add("form-password-hidden");
+  }
+});
+
+//Check password length - Better to add this check to "On Click"
+// otherwise it gets triggered too ofter
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //promazání předchozích hlášek
+  let paragraphs = formAlert.getElementsByTagName("p");
+  console.log(paragraphs);
+  if (paragraphs.length > 0) {
+    while (paragraphs.length > 0) {
+      formAlert.removeChild(paragraphs[0]);
+    }
+  }
+
+  //přidávání hlášek & zobrezní Alertů
+  let lettersCount = formPassword.value.length;
+
+  if (lettersCount > 0 && lettersCount < 9) {
+    formAlert.classList.remove("form-alert-hidden");
+    let pElement = document.createElement("p");
+    pElement.textContent = "Password is too short";
+    formAlert.appendChild(pElement);
+  }
+  // TO DO - kontrola shody hesel
+  if (lettersCount > 0) {
+    formAlert.classList.remove("form-alert-hidden");
+    let pElement2 = document.createElement("p");
+    pElement2.textContent = "Passwords are not matching";
+    formAlert.appendChild(pElement2);
   }
 });
